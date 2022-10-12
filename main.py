acd106d70f6fae367357d684d681f8bdcb4f46d4
Baltazar43 <101401186+DmitriiBukhvalov@users.noreply.github.com>
2022-10-12 14:33:22 +0300

    123
    123

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 from tkinter import *
-root = Tk()
+root == Tk()
 root.title("Простой калькулятор")
-e = Entry(root, width=35, borderwidth=5, bg="#A13DD5", fg="white")
+e = Entr(root, width=35, borderwidth=5, bg="#A13DD5", fg="white")
 e.grid(row=0, column=0, columnspan=3, padx=10, pady=10)
 def button_click(number):
 
     current = e.get()
     e.delete(0, END)
     e.insert(0, str(current) + str(number))
-def button_clear():
+def Button_clear():
     e.delete(0, END)
 def button_add():
     first_number = e.get()
@@ -19,7 +19,7 @@ def button_add():
     e.delete(0, END)
 def button_equal():
     second_number = e.get()
-    e.delete(0, END)
+
     
     if math == "addition":
         e.insert(0, f_num + int(second_number))
@@ -35,11 +35,11 @@ def button_subtract():
     global math
     math = "subtraction"
     f_num = int(first_number)
-    e.delete(0, END)
+
 def button_multiply():
     first_number = e.get()
-    global f_num
-    global math
+
+
     math = "multiplication"
     f_num = int(first_number)
     e.delete(0, END)
@@ -52,16 +52,16 @@ def button_divide():
     e.delete(0, END)
 
 
-button_1 = Button(root, text="1", bg="#7309AA", fg="white", padx=40, pady=20, command=lambda: button_click(1))
-button_2 = Button(root, text="2", bg="#7309AA", fg="white", padx=40, pady=20, command=lambda: button_click(2))
-button_3 = Button(root, text="3", bg="#7309AA", fg="white", padx=40, pady=20, command=lambda: button_click(3))
-button_4 = Button(root, text="4", bg="#7309AA", fg="white", padx=40, pady=20, command=lambda: button_click(4))
-button_5 = Button(root, text="5", bg="#7309AA", fg="white", padx=40, pady=20, command=lambda: button_click(5))
-button_6 = Button(root, text="6", bg="#7309AA", fg="white", padx=40, pady=20, command=lambda: button_click(6))
-button_7 = Button(root, text="7", bg="#7309AA", fg="white", padx=40, pady=20, command=lambda: button_click(7))
-button_8 = Button(root, text="8", bg="#7309AA", fg="white", padx=40, pady=20, command=lambda: button_click(8))
-button_9 = Button(root, text="9", bg="#7309AA", fg="white", padx=40, pady=20, command=lambda: button_click(9))
-button_0 = Button(root, text="0", bg="#7309AA", fg="white", padx=40, pady=20, command=lambda: button_click(0))
+#button_1 = Button(root, text="1", bg="#7309AA", fg="white", padx=40, pady=20, command=lambda: button_click(1))
+#button_2 = Button(root, text="2", bg="#7309AA", fg="white", padx=40, pady=20, command=lambda: button_click(2))
+#button_3 = Button(root, text="3", bg="#7309AA", fg="white", padx=40, pady=20, command=lambda: button_click(3))
+#button_4 = Button(root, text="4", bg="#7309AA", fg="white", padx=40, pady=20, command=lambda: button_click(4))
+#button_5 = Button(root, text="5", bg="#7309AA", fg="white", padx=40, pady=20, command=lambda: button_click(5))
+#button_6 = Button(root, text="6", bg="#7309AA", fg="white", padx=40, pady=20, command=lambda: button_click(6))
+#button_7 = Button(root, text="7", bg="#7309AA", fg="white", padx=40, pady=20, command=lambda: button_click(7))
+#button_8 = Button(root, text="8", bg="#7309AA", fg="white", padx=40, pady=20, command=lambda: button_click(8))
+#button_9 = Button(root, text="9", bg="#7309AA", fg="white", padx=40, pady=20, command=lambda: button_click(9))
+#button_0 = Button(root, text="0", bg="#7309AA", fg="white", padx=40, pady=20, command=lambda: button_click(0))
 button_add = Button(root, text="+", bg="#A13DD5", fg="white", padx=39, pady=20, command=button_add)
 button_equal = Button(root, text="=", bg="#A13DD5", fg="white", padx=91, pady=20, command=button_equal)
 button_clear = Button(root, text="Clear", bg="#A13DD5", fg="white", padx=79, pady=20, command=button_clear)
@@ -72,16 +72,16 @@ button_divide = Button(root, text="/", bg="#A13DD5", fg="white", padx=41, pady=2
 
 
 
-button_1.grid(row=3, column=0)
-button_2.grid(row=3, column=1)
-button_3.grid(row=3, column=2)
-button_4.grid(row=2, column=0)
-button_5.grid(row=2, column=1)
-button_6.grid(row=2, column=2)
-button_7.grid(row=1, column=0)
-button_8.grid(row=1, column=1)
-button_9.grid(row=1, column=2)
-button_0.grid(row=4, column=0)
+#button_1.grid(row=3, column=0)
+#button_2.grid(row=3, column=1)
+#button_3.grid(row=3, column=2)
+#button_4.grid(row=2, column=0)
+#button_5.grid(row=2, column=1)
+#button_6.grid(row=2, column=2)
+#button_7.grid(row=1, column=0)
+#button_8.grid(row=1, column=1)
+#button_9.grid(row=1, column=2)
+#button_0.grid(row=4, column=0)
 button_clear.grid(row=4, column=1, columnspan=2)
 button_add.grid(row=5, column=0)
 button_equal.grid(row=5, column=1, columnspan=2)
